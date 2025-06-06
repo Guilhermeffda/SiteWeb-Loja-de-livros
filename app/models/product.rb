e8987b93d59901413cb07faf09fc966fe30045d8
Guilhermeffda <guiffa05@hotmail.com>
2025-06-06 14:00:05 -0300

    emviando arquivos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,24 @@
+
+
 class Product < ApplicationRecord
+
+    # O validates checa se os atributos estão preenchidos
+        # O presence faz com que o atributo seja obrigatório
+    validates :title, :description, :image_url, presence: true 
+
+    # Aqui fazemos com que para o numero seja so valido se for maior ou igual a 0.01
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+    # Faz com que mão seja permitidos titulos iguais
+    validates :title, uniqueness: true
+
+    # Aqui vai checar se o formato esta entre URL para GIF, JPG ou PNG, caso não for, não sera permitido
+        # Usamos as expressões regulares para validar caracteres apos o ponto
+    validates :image_url, allow_blank: true, format: {
+        with:
+        %r{\.(gif|jpg|png)\z}i,
+        message: 'Deve ser uma URL para GIF, JPG ou PNG.'
+    }
+            #o "\z" indica o fim de uma String, e o "i" transforma todos o caracteres em minúsculos(no caso os que buscamos)
+
 end
