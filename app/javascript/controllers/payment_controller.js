0c07e1f0d754e4d92073500ab52def9005190948
Guilhermeffda <guiffa05@hotmail.com>
2025-06-16 16:47:28 -0300

    Enviando os arquivos do G2, e estou prestes a comecar o G3

---
+++ app/javascript/controllers/payment_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="payment"
+
+//controla a exibição de campos adicionais em um formulário de pagamento.
+
+//verifica o tipo de pagamento selecionado e mostra ou esconde campos extras conforme necessário.
+export default class extends Controller {
+  static targets = [ "selection", "additionalFields" ]
+
+  initialize(){
+    this.showAdditionalFields()
+  }
+
+  showAdditionalFields(){
+    let Selection = this.selectionTarget.value
+
+    for (let fields of tthis.additionalFieldsTargets){
+      fields.disabled = fields.hidden = (fields.dataset.type != selection)
+    }
+  }
+}
