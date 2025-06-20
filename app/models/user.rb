ca60624058b2e2888c0ae1a30c6ea514cd850641
Guilhermeffda <guiffa05@hotmail.com>
2025-06-20 13:59:57 -0300

    finalizando o capitulo 14 e enviando o I4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
+
+
+
 class User < ApplicationRecord
   # O has_secure_password() garante que o modelo User
   # tenha funcionalidades de autenticação, como validação de senha
   # e geração de hash de senha.
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  class Error < StandardError; end
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise Error.new("Can't delete last user")
+    end
+  end
 end
